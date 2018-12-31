23e2d1f20366f4f1b3a14f5e51734cd95c28f33a
TanjotP <tanjotspanesar@gmail.com>
2018-12-30 22:16:49 -0500

    initial video impl... needs refactor and correct assets

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+var video = document.getElementById("bgvid"),
+pauseButton = document.querySelector("#polina button");
 
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });	
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
 
+function vidFade() {
+  video.classList.add("stopfade");
+}
 
-});
+video.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+video.pause();
+vidFade();
+}, false); 
+ 
+pauseButton.addEventListener("click", function() {
+  video.classList.toggle("stopfade");
+  if (video.paused) {
+    video.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    video.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+}, false);
 
-
-
-
-
-
+video.addEventListener('touchstart', function(e) {
+e.preventDefault();
+video.play();
+})
